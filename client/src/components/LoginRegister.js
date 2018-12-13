13817e1fc4b0ca52965f2b092c9a03e2fed45cca
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-12-13 13:50:33 -0500

    styles LoginRegister component

--- client/src/components/LoginRegister.js
+++ client/src/components/LoginRegister.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import logo from '../logo.png';
+
 class LoginRegister extends React.Component {
     constructor(props) {
         super(props);
@@ -10,6 +12,7 @@ class LoginRegister extends React.Component {
             confirmedPassword: false,
             canSubmit: false,
             username: '',
+            usernameChecked: false,
             password: '',
             confirmPassword: ''
         };
@@ -26,8 +29,15 @@ class LoginRegister extends React.Component {
 
     // checks to see if username is already in database, returns true if available for new account
     availableUsername = async (username) => {
+        if(username === '') {
+            this.setState({ usernameChecked: false});
+            return;
+        }
         try {
             const valid = await axios.post('https://tennis-notes.herokuapp.com/api/users/availableUsername', { username: username });
+            if(!this.state.usernameChecked) {
+                this.setState({ usernameChecked: true });
+            }
             return valid.data;
         } catch (err) {
             console.log(err);
@@ -76,13 +86,15 @@ class LoginRegister extends React.Component {
 
     // toggles view between register and login
     toggleLoginRegister = () => {
-        this.setState(previousState => { 
-            return { 
+        this.setState(previousState => {
+            return {
                 isLogin: !previousState.isLogin,
                 username: '',
                 password: '',
-                confirmPassword: ''
-            }});
+                confirmPassword: '',
+                usernameChecked: false
+            }
+        });
     }
 
     // sumbit handler for register view, new username and password to db
@@ -95,7 +107,7 @@ class LoginRegister extends React.Component {
 
         axios.post('https://tennis-notes.herokuapp.com/api/users/register', newUser)
             .then(response => {
-                if(response.rowCount){
+                if (response.rowCount) {
                     console.log('YAYAYAYAYAYAYA!');
                 } else {
                     console.log('Error registering');
@@ -108,37 +120,46 @@ class LoginRegister extends React.Component {
 
     render() {
         return (
-            <div className="login-register-wrapper">
-                <form onSumbit={this.state.isLogin ? null : this.registerSubmit} className={this.state.isLogin ? "login-form" : "register-form"}>
-                    <input
-                        type="text"
-                        name="username"
-                        className="username-input"
-                        onChange={this.changeHandler}
-                        placeholder="Username"
-                        value={this.state.username}
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        className="password-input"
-                        onChange={this.changeHandler}
-                        placeholder="Password"
-                        value={this.state.password}
-                    />
-                    {this.state.isLogin ? null : <input
-                        type="password"
-                        name="confirmPassword"
-                        className="password-input"
-                        onChange={this.changeHandler}
-                        placeholder="Confirm Password"
-                        value={this.state.confirmPassword}
-                    />
-                    }
-                    <button type="button" onClick={this.state.isLogin ? null : this.registerSubmit} className={this.state.isLogin ? "login-button" : "register-button"} disabled={this.state.isLogin ? false : (this.state.canSubmit ? false : true)}>Submit</button>
-                </form>
-                <span onClick={this.toggleLoginRegister}>Toggle!</span>
-            </div >
+            <div className="centered-page">
+                <div className="login-register-wrapper">
+                    <img src={logo} alt="logo" className="login-register-logo" />
+                    <form autocomplete="off" onSumbit={this.state.isLogin ? null : this.registerSubmit} className={this.state.isLogin ? "login-form" : "register-form"}>
+                        <label className={`login-register-form-label ${this.state.usernameChecked && this.state.validUsername ? 'valid' : (this.state.usernameChecked ? 'invalid' : null)}`} for="username">USERNAME</label>
+                        <input
+                            type="text"
+                            name="username"
+                            className="username-input"
+                            onChange={this.changeHandler}
+                            placeholder="Username"
+                            value={this.state.username}
+                        />
+                        <label className="login-register-form-label" for="password">PASSWORD</label>
+                        <input
+                            type="password"
+                            name="password"
+                            className="password-input"
+                            onChange={this.changeHandler}
+                            placeholder="Password"
+                            value={this.state.password}
+                        />
+                        {this.state.isLogin ? null :
+                            <div className="confirm-wrapper">
+                                <label className="login-register-form-label" for="confirmPassword">CONFIRM PASSWORD</label>
+                                <input
+                                    type="password"
+                                    name="confirmPassword"
+                                    className="password-input"
+                                    onChange={this.changeHandler}
+                                    placeholder="Confirm Password"
+                                    value={this.state.confirmPassword}
+                                />
+                            </div>
+                        }
+                        <button type="button" onClick={this.state.isLogin ? null : this.registerSubmit} className={this.state.isLogin ? "login-button" : (this.state.canSubmit ? "register-button" : "register-button-disabled")} disabled={this.state.isLogin ? false : (this.state.canSubmit ? false : true)}>{this.state.isLogin ? 'Login' : 'Register'}</button>
+                    </form>
+                    <span onClick={this.toggleLoginRegister}>{this.state.isLogin ? 'REGISTER NEW USER' : 'LOGIN TO ACCOUNT'}</span>
+                </div>
+            </div>
         )
     }
 }
